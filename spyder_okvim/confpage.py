ae03b26c6530ee03596386dc073a22251e005004
ok97465 <ok97465@gmail.com>
2020-11-16 22:31:09 +0900

    improve statement of confpage

--- spyder_okvim/confpage.py
+++ spyder_okvim/confpage.py
@@ -20,7 +20,7 @@ class OkvimConfigPage(PluginConfigPage):
         newce = self.create_coloredit
         newsb = self.create_spinbox
 
-        color_group = QGroupBox("Color")
+        color_group = QGroupBox('Color')
         color_layout = QGridLayout()
 
         conf_color_info = {
@@ -30,27 +30,29 @@ class OkvimConfigPage(PluginConfigPage):
                 'Selection Bg': ('select_bg_color', 1, 2),
                 'Search Fg': ('search_fg_color', 2, 0),
                 'Search Bg': ('search_bg_color', 2, 2),
-                'Yank Fg': ('yank_fg_color', 3, 0),
-                'Yank Bg': ('yank_bg_color', 3, 2),
+                'Highlight after yank Fg': ('yank_fg_color', 3, 0),
+                'Highlight after yank Bg': ('yank_bg_color', 3, 2),
                 }
 
         for name, (option_name, idx_row, idx_col) in conf_color_info.items():
             label, clayout = newce(name, option_name, without_layout=True)
             label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
             clayout.lineedit.setValidator(
-                    QRegExpValidator(QRegExp(r"#[0-9abcdefABCDEF]{6}")))
+                    QRegExpValidator(QRegExp(r'#[0-9abcdefABCDEF]{6}')))
             color_layout.addWidget(label, idx_row, idx_col)
             color_layout.addLayout(clayout, idx_row, idx_col + 1)
             color_group.setLayout(color_layout)
 
-        options_group = QGroupBox("Options")
+        options_group = QGroupBox('Options')
         options_layout = QVBoxLayout()
-        options_layout.addWidget(newcb("ignorecase", "ignorecase"))
-        options_layout.addWidget(newcb("smartcase", "smartcase"))
+        options_layout.addWidget(newcb('ignorecase', 'ignorecase'))
+        options_layout.addWidget(newcb('smartcase', 'smartcase'))
 
         hl_yank_layout = QHBoxLayout()
-        hl_yank_layout.addWidget(newcb("highlight yank", "highlight_yank"))
-        hl_yank_layout.addWidget(newsb(None, 'ms', "highlight_yank_duration",
+        hl_yank_layout.addWidget(newcb('highlight after yank',
+                                       'highlight_yank'))
+        hl_yank_layout.addWidget(newsb('(duration ', 'ms)',
+                                       'highlight_yank_duration',
                                        min_=0, max_=2000, step=100))
         options_layout.addLayout(hl_yank_layout)
 
